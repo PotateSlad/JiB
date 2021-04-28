186104d915c51e3f174e1f0c06a5c9e88cf6b75d
Wm. Nicholas French <wnfrench@mac-air-35.local>
2021-04-28 14:50:29 -0500

    options menu

--- JiB/Assets/Scripts/Settings.cs
+++ JiB/Assets/Scripts/Settings.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class Settings : MonoBehaviour
 {
-    public void Play()
+    public void Resume()
     {
-        //set variables to starting levels
+        //reload game as it was
         SceneManager.LoadScene(1); //https://www.google.com/search?q=make+new+game+screen+unity&rlz=1C5GCEM_enUS942US942&ei=rDVnYN-gB8artQbK7YWgDw&oq=make+new+game+screen+unity&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsAM6BAghEApQvRhYkTtg8j1oAnACeACAAbIBiAHvB5IBAzcuM5gBAKABAaoBB2d3cy13aXrIAQi4AQLAAQE&sclient=gws-wiz&ved=0ahUKEwjfpubV7d_vAhXGVc0KHcp2AfQQ4dUDCA0&uact=5#kpvalbx=_2DVnYIeoLZn5tAbA2p2YDw12
     } 
-    public void LoadGame()
+    public void Save()
     {
-        //put load game functions here
-
-        SceneManager.LoadScene(1); //https://www.google.com/search?q=make+new+game+screen+unity&rlz=1C5GCEM_enUS942US942&ei=rDVnYN-gB8artQbK7YWgDw&oq=make+new+game+screen+unity&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsAM6BAghEApQvRhYkTtg8j1oAnACeACAAbIBiAHvB5IBAzcuM5gBAKABAaoBB2d3cy13aXrIAQi4AQLAAQE&sclient=gws-wiz&ved=0ahUKEwjfpubV7d_vAhXGVc0KHcp2AfQQ4dUDCA0&uact=5#kpvalbx=_2DVnYIeoLZn5tAbA2p2YDw12
+        //put save game functions here
     }
     public void Quit()
     {
         Debug.Log("Quit"); //https://www.google.com/search?q=make+new+game+screen+unity&rlz=1C5GCEM_enUS942US942&ei=rDVnYN-gB8artQbK7YWgDw&oq=make+new+game+screen+unity&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsAM6BAghEApQvRhYkTtg8j1oAnACeACAAbIBiAHvB5IBAzcuM5gBAKABAaoBB2d3cy13aXrIAQi4AQLAAQE&sclient=gws-wiz&ved=0ahUKEwjfpubV7d_vAhXGVc0KHcp2AfQQ4dUDCA0&uact=5#kpvalbx=_2DVnYIeoLZn5tAbA2p2YDw12
         Application.Quit(); //https://www.google.com/search?q=make+new+game+screen+unity&rlz=1C5GCEM_enUS942US942&ei=rDVnYN-gB8artQbK7YWgDw&oq=make+new+game+screen+unity&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsAM6BAghEApQvRhYkTtg8j1oAnACeACAAbIBiAHvB5IBAzcuM5gBAKABAaoBB2d3cy13aXrIAQi4AQLAAQE&sclient=gws-wiz&ved=0ahUKEwjfpubV7d_vAhXGVc0KHcp2AfQQ4dUDCA0&uact=5#kpvalbx=_2DVnYIeoLZn5tAbA2p2YDw12
-    }  
+    } 
 }
